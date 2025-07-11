cb77974920f9f7a80eaa9f783ab80a4390c09e94
Govind Dandekar <govind.dandekar@gmail.com>
2025-07-11 14:43:34 -0700

    update HomePage with animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,23 @@
 "use client"
 
-// import { RadixProgress } from "@/app/components/ui/radix-progress"
-// import Typewriter from "./components/ui/typewriter";
+import { RadixProgress } from "@/app/components/ui/radix-progress"
+import Typewriter from "./components/ui/typewriter";
 
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  // const [showRadixProgress, setShowRadixProgress] = useState(true)
+  const [showRadixProgress, setShowRadixProgress] = useState(true)
   
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setShowRadixProgress(false)
-  //   }, 3300)
-  // }, [])
+  useEffect(() => {
+    setTimeout(() => {
+      setShowRadixProgress(false)
+    }, 3300)
+  }, [])
   
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center -mt-16 min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        {/* {showRadixProgress ? <RadixProgress /> : <Typewriter />} */}
+        {showRadixProgress ? <RadixProgress /> : <Typewriter />}
         Hello World!
       </main>
     </div>
